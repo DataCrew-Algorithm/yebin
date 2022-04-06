92a83b80758082592811b2887ce0d6364991c119
Yeb-In <wyb9712@gmail.com>
2022-04-06 13:20:08 +0900

    first commit

---
+++ Apr_06_22.py
@@ -0,0 +1,16 @@
+
+l = []
+ind = [] #index
+for _ in range(5):
+    l.append(input())
+
+for i in l:
+    if 'FBI' in i:
+       ind.append((l.index(i)+1))
+
+if len(ind) > 0:
+    ind.sort()
+    ind = ' '.join(map(str, ind))
+    print(ind)
+else:
+    print("HE GOT AWAY!")
