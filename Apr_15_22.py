78ffc7a93e3c97686c288e72481f916640535335
Yeb-In <wyb9712@gmail.com>
2022-04-15 11:34:38 +0900

    second / annotation

--- Apr_15_22.py
+++ Apr_15_22.py
@@ -7,8 +7,8 @@ color = ['black','brown','red','orange','yellow',
 'green','blue','violet','grey','white']
 
 for _ in range(3):
-    idx = color.index(input())
-    r.append(idx)
+    idx = color.index(input()) #yellow violet red
+    r.append(idx) # 4 7 2
 
-resistance = int(str(r[0])+str(r[1])) * (10**r[2]) 
-print(resistance)
+resistance = int(str(r[0])+str(r[1])) * (10**r[2]) #('4' + '7') -> 47 * 10^2
+print(resistance) #4700
