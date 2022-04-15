626b6f1798f2967d1a8f7aa27845975aa3611a7a
Yeb-In <wyb9712@gmail.com>
2022-04-15 19:39:01 +0900

    third commit

--- Apr_15_22.py
+++ Apr_15_22.py
@@ -12,3 +12,7 @@ for _ in range(3):
 
 resistance = int(str(r[0])+str(r[1])) * (10**r[2]) #('4' + '7') -> 47 * 10^2
 print(resistance) #4700
+
+#요셉님 코드(dictionary)
+reg_color = {'black':0, 'brown':1, 'red':2, 'orange':3, 'yellow':4, 'green':5, 'blue':6, 'violet':7, 'grey':8, 'white':9}
+print((10*reg_color[input()]+reg_color[input()])*10**reg_color[input()])
