83d66f7bc6fd7b6d7d3b16931c3c0946c4bd0ef6
Yeb-In <wyb9712@gmail.com>
2022-04-19 21:14:10 +0900

    2nd commit

--- Apr_19_22.py
+++ Apr_19_22.py
@@ -1,3 +1,4 @@
+#BOJ2748
 #피보나치 수2
 
 seq = [0,1]
@@ -6,6 +7,5 @@ n = int(input())
 for i in range(2,n+1): # n>=2
     num = (seq[i-1] + seq[i-2])
     seq.append(num)
-    i += 1
 print(seq[n])
     
