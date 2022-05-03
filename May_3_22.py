7f5a218a7e143b86b58dd6e8ec1d725c8aa904fe
Yeb-In <wyb9712@gmail.com>
2022-05-03 18:12:51 +0900

    Leet 819. Most Common Word

--- May_3_22.py
+++ May_3_22.py
@@ -1,6 +1,5 @@
-#leet819
 #Most common Word
-
+#v1 'run != submit'
 class Solution:
     def mostCommonWord(self, paragraph, banned):
         punc = ",."
@@ -19,4 +18,21 @@ banned = ["hit"]
 sol = Solution()
 print(sol.mostCommonWord(paragraph, banned))
     
-            
+#v2 60ms
+class Solution:
+    def mostCommonWord(self, paragraph, banned):
+        punc = ",.!'?;`'" #비효율적 -> 정규표현식 사용하는거 추천
+        for punc in punc:
+            paragraph = (paragraph.lower()).replace(punc," ")
+        word = paragraph.split()
+        r_word = [x for x in word if x not in banned]
+        lst = []
+        for i in r_word:
+            n = r_word.count(i)
+            lst.append(n)
+        return r_word[lst.index(max(lst))]
+
+paragraph = "a, a, a, a, b,b,b,c, c"
+banned = ["a"]
+sol = Solution()
+print(sol.mostCommonWord(paragraph, banned))
