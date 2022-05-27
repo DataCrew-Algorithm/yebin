98663b1858d71a4c1fc222a2f7a021debe384c53
Yeb-In <wyb9712@gmail.com>
2022-05-27 18:46:46 +0900

    콘서트

--- May_27_22.py
+++ May_27_22.py
@@ -1,29 +1,25 @@
-#BOJ12605
-#단어순서 뒤집기
-#64ms
+#BOJ16446
+#콘서트
+#가고싶다
 
-'''스페이스로 띄어쓰기 된 단어들의 리스트가 주어질때, 단어들을 반대 순서로 뒤집어라.
-각 라인은 w개의 영단어로 이루어져 있으며, 총 L개의 알파벳을 가진다.
-각 행은 알파벳과 스페이스로만 이루어져 있다.
-단어 사이에는 하나의 스페이스만 들어간다.'''
+#v1 532ms
+import sys
+n= int(sys.stdin.readline())
+nums = set([i for i in range(1,n+1)])
+first_ticket = set(list(map(int, sys.stdin.readline().split())))
 
-#input
-# 3
-# this is a test
-# foobar
-# all your base
+if (nums - first_ticket) == set(): #빈 집합 나오는 경우
+    print(n+1)
+else:
+    print(min(nums - first_ticket))
 
-#output
-# Case #1: test a is this
-# Case #2: foobar
-# Case #3: base your all
-# print(f'Case #{}: {}',i+1,바뀐문장)
+#v2 556ms
 
+n = int(input())
+nums = set([i for i in range(1,n)])
+first_ticket = set(list(map(int, input().split())))
 
-for i in range(int(input())):
-    lst = input().split()
-    f_lst = []
-    for j in range(len(lst)):
-        f_lst.append(lst.pop())
-    sentence = " ".join(f_lst)
-    print(f'Case #{i+1}: {sentence}')
+if (nums - first_ticket) == set(): #빈 집합 나오는 경우
+    print(n+1)
+else:
+    print(min(nums - first_ticket))
