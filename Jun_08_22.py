e43467b8fd1af030f433567794e60dcfc4b9527f
Yeb-In <wyb9712@gmial.com>
2022-06-09 00:44:06 +0900

    Min Max heapq

---
+++ Jun_08_22.py
@@ -0,0 +1,34 @@
+#BOJ1927
+#최소 힙
+#156ms
+
+import heapq
+import sys
+
+heap = []
+for _ in range(int(sys.stdin.readline())):
+    x = int(sys.stdin.readline())
+    if x!= 0:
+        heapq.heappush(heap,x)
+    else:
+        if heap == []:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+
+#BOJ11279
+#최대 힙
+
+import heapq
+import sys
+
+heap = []
+for _ in range(int(sys.stdin.readline())):
+    x = int(sys.stdin.readline())
+    if x!= 0:
+        heapq.heappush(heap,-x)
+    else:
+        if heap == []:
+            print(0)
+        else:
+            print(-(heapq.heappop(heap)))
